b33ffa49ebf4632b9cbda73e8a9a22e5c32a3899
Alex <temper8@gmail.com>
2021-10-02 04:15:00 +0300

    Create race.py

---
+++ astra/race.py
@@ -0,0 +1,70 @@
+import astra_zip
+
+from zipfile import ZipFile
+
+import matplotlib.pyplot as plt
+
+def get_traj_list(f):
+    tmp = 'lhcd/out/traj.'
+    with ZipFile('races/'+ f) as zip:
+        return [ z.filename for z in zip.filelist if (z.filename.startswith(tmp))]
+
+def float_try(str):
+    try:
+        return float(str)
+    except ValueError:
+        return 0.0
+
+
+class Race:
+    file = ""
+    astra_config = []
+    rt_config = []
+    traj_list = []
+    def __init__(self, f):
+        self.file = f
+        self.astra_config =  astra_zip.get_astra_config(f)
+        self.traj_list = get_traj_list(f)
+        print('init')
+
+    def print_summary(self):
+        print(" ======  ASTRA summary =====")
+        astra_home = self.astra_config['Astra config']['astra_path']
+        exp_file = self.astra_config['Astra config']['exp_file']
+        equ_file = self.astra_config['Astra config']['equ_file']
+    
+        print('{0:12}  {1}'.format(astra_home[1], astra_home[0]))
+        print('{0:12}  {1}'.format(exp_file[1], exp_file[0]))
+        print('{0:12}  {1}'.format(equ_file[1], equ_file[0]))
+
+    def read_trajectories(self, f):
+        with ZipFile('races/'+ self.file) as zip:
+            with zip.open(f) as file:
+                header = file.readline().decode("utf-8").replace('=', '_').split()
+
+                lines = file.readlines()
+                table = [line.decode("utf-8").split() for line in lines]
+                table = list(filter(None, table))
+
+                rays = []
+                N_traj = 0
+
+                for row in table:
+                    if N_traj != int(row[12]):
+                        N_traj = int(row[12])
+                        ray = dict([ (h, []) for h in header ])
+                        rays.append(ray)
+                    for index, (p, item) in enumerate(ray.items()):
+                        item.append(float_try(row[index]))
+        return rays, N_traj
+
+    def plot_trajectory(self, f):
+        rays, max_N_traj = self.read_trajectories(f)
+        print("Number of traj "+ str(len(rays)) + "   Max N_traj "+ str(max_N_traj))
+        plt.figure(figsize=(6,6))
+        #R, Z = read_bounds("out/lcms.dat")
+        #plt.plot(R, Z)
+        for ray in rays:
+            plt.plot(ray['R'], ray['Z'], alpha=0.5, linewidth=1)
+        plt.show()
+
