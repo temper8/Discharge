7b0581a3f23e1848985854db965b3d1ed124f35c
Alex <temper8@gmail.com>
2021-10-02 04:15:48 +0300

    Update summary.py

--- astra/summary.py
+++ astra/summary.py
@@ -4,22 +4,30 @@ from IPython.display import display
 from ipywidgets.widgets.widget_button import ButtonStyle
 from ipywidgets.widgets.widget_layout import Layout
 from ipywidgets.widgets.widget_output import Output
+from race import Race
 
 output = []
 
 race_file = ''
 
+R1 = []
+
 def info():
     print(race_file)
+    R1.print_summary()
 
 def widget():
     global output
+    global R1
     output = widgets.Output()
     filenames = next(os.walk(os.path.abspath('races/')), (None, None, []))[2]
 
     def on_value_change(change):
         global race_file
+        global R1
         race_file = change['new']
+        R1 = Race(race_file)
+        output.clear_output()
         with output:
             print(race_file)
 
