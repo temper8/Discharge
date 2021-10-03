31c94ed5f490dc54305b2253d9472d4bda4a05af
Alex <temper8@gmail.com>
2021-10-04 00:30:34 +0300

    print time

--- astra/sbr/debug.f
+++ astra/sbr/debug.f
@@ -1,4 +1,7 @@
       subroutine debug
       implicit none
-      print *, "test"
+      include 'for/parameter.inc'
+      include 'for/const.inc'
+      include 'for/status.inc'      
+      print *, "time", time
       end subroutine
